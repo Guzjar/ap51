d129be3c39d6ed41cd4514464aabac82867e5876
ANGEL SANTIAGO GUZMAN JARAMILLO <anguja@alumnatflorida.es>
2025-11-04 12:37:26 +0100

    ejercicio 4 no terminado

--- ejercicio4.php
+++ ejercicio4.php
@@ -1,1 +1,19 @@
 <?php
+
+$empleados = [
+   'Juan'=>[
+      'horas' => 40,
+      'tarifa' => 15
+    ],
+   'Perico'=>[
+      'horas' => 20,
+      'tarifa' => 25
+    ],
+   'Andrés'=>[
+      'horas' => 45,
+      'tarifa'=>
+    ],
+];
+function calculaSalario(){
+    $empleados[0[0]]    
+}
